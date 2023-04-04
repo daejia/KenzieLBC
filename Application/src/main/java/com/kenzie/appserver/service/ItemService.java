9dd5c65da907e2096e98de442ee92d7fd12a6f71
cwilliamson51993 <cameron.williamson@mykenzie.snhu.edu>
2023-04-03 21:06:14 -0400

    updateCartItem changes

--- Application/src/main/java/com/kenzie/appserver/service/ItemService.java
+++ Application/src/main/java/com/kenzie/appserver/service/ItemService.java
@@ -24,7 +24,6 @@ public class ItemService {
     }
 
     public void updateCartItem(Item item) {
-
             ItemRecord itemRecord = new ItemRecord();
             itemRecord.setId(item.getId());
             itemRecord.setStore(item.getStore());
@@ -49,16 +48,5 @@ public class ItemService {
         return item;
     }
 
-    public void updateCartItem(Item item) {
-            ItemRecord itemRecord = new ItemRecord();
-            itemRecord.setId(item.getId());
-            itemRecord.setStore(item.getStore());
-            itemRecord.setBrandType(item.getBrandType());
-            itemRecord.setName(item.getName());
-            itemRecord.setCategory(item.getCategory());
-            itemRecord.setPrice(item.getPrice());
-            itemRecord.setInStock(item.getIsInStock());
-            itemRepository.save(itemRecord);
-        }
     }
 
