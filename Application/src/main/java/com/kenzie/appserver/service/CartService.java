bc7aedaa511abbf65bab10ccfa778213fa07c56a
cwilliamson51993 <cameron.williamson@mykenzie.snhu.edu>
2023-04-08 13:24:01 -0400

    commit

--- Application/src/main/java/com/kenzie/appserver/service/CartService.java
+++ Application/src/main/java/com/kenzie/appserver/service/CartService.java
@@ -62,18 +62,6 @@ public class CartService {
         return cart.getItems().get(item);
     }
 
-    public boolean updateCartItem(String itemId, Integer itemsUpdate) {
-        Optional<Item> itemOptional = ItemRepository.findById(itemId);
-        if (itemOptional.isPresent()) {
-            Item item = itemOptional.get();
-            item.setQuantity(newQuantity);
-            ItemRepository.save(item);
-            return true;
-        } else {
-            return false;
-        }
-
-    }
 
     public class CartNotFoundException extends Throwable {
         public CartNotFoundException(String message) {
